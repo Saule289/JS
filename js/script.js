30be4af6c548a64185d62a536903a99cbfb01dd1
saule <saule289@mail.ru>
2022-06-16 21:54:12 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+ "use strict"; 
+
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// let username = "Saule";
+// // let name2 = "Saule";
+
+
+
+// let age = 36;
+//  //console.log(age);
+ 
+//  let person = username + " " + age;
+// //  console.log(person); 
+
+//  let user_age = 30
+//  let work_period = 10
+
+//  let total_time = user_age - work_period;
+// //  console.log("total_time =" + total_time);
+
+//  let morning = false;
+//  //console.log("morning = ", morning, typeof(morning));
+//  let evening = true;
+//  //console.log("evening = ", evening, typeof(evening));
+
+// //  let apples = 15;
+// //  let cherry = 5;
+
+// //  let compare = apples >= cherry;
+// //  console.log ("compare =", compare);
+
+// //  if (false) {
+// //     console.log("Compare is", compare)
+// //  } else if (apples == 15) {
+// //     console.log ("apples == ", apples);
+// //  }
+// // else {
+// //     console.log ("!!compare =", false);
+// // }
+
+
+// alert('Hello');
+// const result = confirm("Are you here?")
+//  const answer = +prompt("How are you old?", "");
+//  console.log(typeof(answer));
+
+// const answers =[];
+// answers[0] =prompt("What's your name?", "");
+// answers[1] =prompt("What's your surname?", "");
+// answers[2] =prompt("How ol are you?", "");
+
+// document.write(answers);
+ console.log('arr' + " -object");
+
+ let incr = 10,
+     decr = 10;
+
+     ++incr;
+     --decr;
+     console.log(incr);
+     console.log(decr);
+
+
+
+
+
+
+
+
