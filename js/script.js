bb6d831683f5b39a3f70a99bd0e29a6ed9b6bee8
saule <saule289@mail.ru>
2022-06-17 16:48:24 +0400

    headers

--- js/script.js
+++ js/script.js
@@ -1,73 +1,36 @@
- "use strict"; 
 
-// let number = 4.6;
+// console.log('hello')
 
-// console.log(4/0);
-// console.log('string' * 9);
+const { INTERNAL_COMPUTE_OFFSET_SCRIPT } = require("selenium-webdriver/lib/input");
 
-// let username = "Saule";
-// // let name2 = "Saule";
+// console.log(true && false || true)
 
+// if (false && true || false) {
+//     console.log('1')
+// } else{
+//     console.log('2')
+// }
 
+let dogshow = true
+let age = 17;
+let money = 700;
+let dog = true;
 
-// let age = 36;
-//  //console.log(age);
- 
-//  let person = username + " " + age;
-// //  console.log(person); 
-
-//  let user_age = 30
-//  let work_period = 10
-
-//  let total_time = user_age - work_period;
-// //  console.log("total_time =" + total_time);
-
-//  let morning = false;
-//  //console.log("morning = ", morning, typeof(morning));
-//  let evening = true;
-//  //console.log("evening = ", evening, typeof(evening));
-
-// //  let apples = 15;
-// //  let cherry = 5;
-
-// //  let compare = apples >= cherry;
-// //  console.log ("compare =", compare);
-
-// //  if (false) {
-// //     console.log("Compare is", compare)
-// //  } else if (apples == 15) {
-// //     console.log ("apples == ", apples);
-// //  }
-// // else {
-// //     console.log ("!!compare =", false);
-// // }
+// if (age >= 18 && money ==1000 &&  dog) {
+//     console.log("Get ticket")
+// } else {if (age < 18) {
+//     console.log("Early")
+// }  if (money <1000) {
+// console.log("Keep calm and work hard!!")
+// }  if (!dog) {
+//     console.log("Get a cat")
+// } }
 
 
-// alert('Hello');
-// const result = confirm("Are you here?")
-//  const answer = +prompt("How are you old?", "");
-//  console.log(typeof(answer));
-
-// const answers =[];
-// answers[0] =prompt("What's your name?", "");
-// answers[1] =prompt("What's your surname?", "");
-// answers[2] =prompt("How ol are you?", "");
-
-// document.write(answers);
- console.log('arr' + " -object");
-
- let incr = 10,
-     decr = 10;
-
-     ++incr;
-     --decr;
-     console.log(incr);
-     console.log(decr);
-
-
-
-
-
+switch (dogshow) {
+    case age >=18:
+        console.log("Time to win")
+       }
 
 
 
