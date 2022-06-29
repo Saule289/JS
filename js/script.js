78bcda32ea29c822dcc3248c771dc30954bf678a
saule <saule289@mail.ru>
2022-06-29 16:43:17 +0400

    Functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+"use strict"
 
-'use strict';
+let numberOfFilms;
 
-const numberOfFilms = +prompt ('Сколько фильмов ввы уже посмотрели?', '');
+function start() {
+numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
+
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+              numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
+   }
+   
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,6 +20,8 @@ const personalMovieDB = {
    privat: false 
 };
  
+
+function rememberMyFilms() {
 
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
@@ -24,7 +35,12 @@ for (let i = 0; i < 2; i++) {
             i--;
           }
           
-}; 
+}
+}
+
+rememberMyFilms();
+
+function detectPersonallevel() {
  if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов')
  } else if (personalMovieDB.count <= 30 && personalMovieDB.count >= 10) {
@@ -33,5 +49,40 @@ for (let i = 0; i < 2; i++) {
     console.log('Вы киноман')
  } else {
     console.log('Произошла ошибка')
- };
+ }
+}
+
+detectPersonallevel();
+ 
+
+function showMyDB(hidden) {
+   if (!hidden) {
+   console.log(personalMovieDB)
+}
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+   for (let i = 1; i <= 3; i++) {
+      const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+      personalMovieDB.genres[i - 1]
+            }
+   }
+   writeYourGenres(); 
+   
 console.log(personalMovieDB); 
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+// Код возьмите из предыдущего домашнего задания
